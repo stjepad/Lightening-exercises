742c39937750b23c0c84e058b316094d10baeaa2
Dejan Stjepanovic <ds2q88@gmail.com>
2018-07-25 11:12:32 -0500

    attached lightening 4 to dom

--- 4/main.js
+++ 4/main.js
@@ -1,3 +1,7 @@
+
+
+const fragment = document.createDocumentFragment()
+
 // Lightning Exercise Loop over every item in this array
 //  ```[45, "I", true, null, "am", 3.56, "a", undefined, { catchphrase: "Oh hai, Mark"}, "JS", "rockstar"]```
 // and return a new array that contains only the strings. Log out the result.
@@ -10,6 +14,33 @@ array = [45, "I", true, null, "am", 3.56, "a", undefined, { catchphrase: "Oh hai
 array.forEach(element => {if(typeof(element)=== "string"){
     stringArray.push(element)
 }})
-console.log(stringArray)
+// console.log(stringArray)
+const DOM = document.createElement("div")
+DOM.textContent = stringArray.join('' + " ");
+fragment.appendChild(DOM)
+// console.log("bloop", fragment)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const arrayDiv = document.getElementById("ArrayDiv");
+arrayDiv.appendChild(fragment)
+// console.log(fragment)
+// console.log(stringArray.join(''));
+
+
 
 
